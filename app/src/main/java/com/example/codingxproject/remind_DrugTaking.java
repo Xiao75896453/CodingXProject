2c624470ba61c14c19ddca096623f312efc95749
xiao75896453 <a75896453@mail.com>
2019-08-26 21:59:15 +0800

    Xiao_remindUI_T_D_R

---
+++ app/src/main/java/com/example/codingxproject/remind_DrugTaking.java
@@ -0,0 +1,14 @@
+package com.example.codingxproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class remind_DrugTaking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remind__drug_taking);
+    }
+}
---
+++ app/src/main/java/com/example/codingxproject/remind_takeMedicine.java
@@ -0,0 +1,14 @@
+package com.example.codingxproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class remind_takeMedicine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remind_take_medicine);
+    }
+}

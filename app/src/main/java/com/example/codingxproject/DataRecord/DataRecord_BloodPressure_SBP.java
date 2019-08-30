87ce8ac3ee50925ba72e95e308c20253a850066d
ElizaHuang <eliza85827@gmail.com>
2019-08-30 21:23:12 +0800

    修改新增數據頁面- 預設時間顯示目前手機時間   更改時間會跳出浮動視窗(可以用時鐘點選，或用鍵盤輸入)

--- app/src/main/java/com/example/codingxproject/DataRecord/DataRecord_BloodPressure_SBP.java
+++ app/src/main/java/com/example/codingxproject/DataRecord/DataRecord_BloodPressure_SBP.java
@@ -22,59 +22,90 @@ public class DataRecord_BloodPressure_SBP extends AppCompatActivity {
     private Button bTimeChange ;
     private  static final int REQUEST_CODE =1 ;
     private NumberPicker mNumberPicker;
-
+    final static Calendar currentCalendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_record_blood_pressure_sbp);
-        tvTime= (TextView)findViewById(R.id.tvTime);
-        bTimeChange = (Button)findViewById(R.id.tvChangeTime);
-        Intent intent_SBP = getIntent();
-        startActivityForResult(intent_SBP,REQUEST_CODE);
-        bTimeChange.setOnClickListener(new View.OnClickListener() {
+        String currentHour, currentMin;
+        currentHour = setTimeForm(currentCalendar.get(Calendar.HOUR_OF_DAY));
+        currentMin = setTimeForm(currentCalendar.get(Calendar.MINUTE));
+        String currentTime = currentHour + ":" + currentMin;
+
+        final TextView tvCurrentTime = (TextView) findViewById(R.id.tvTime);
+        tvCurrentTime.setText(currentTime);
+
+        Button resetTime = (Button) findViewById(R.id.bChangeTime);
+        resetTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //預設為系統時間
-                final Calendar c = Calendar.getInstance();
-                int hour = c.get(Calendar.HOUR_OF_DAY);
-                int minute = c.get(Calendar.MINUTE);
-                //抓現在時間
                 new TimePickerDialog(DataRecord_BloodPressure_SBP.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onTimeSet(TimePicker timePicker, int houreOfDay, int minute) {
-                        tvTime.setText("現在時間是" + houreOfDay + ":" + minute);
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMin) {
+                        String hour = setTimeForm(selectedHour);
+                        String min = setTimeForm(selectedMin);
+                        tvCurrentTime.setText(hour+":"+min);
                     }
-                }, hour,minute,true).show();
+                }, currentCalendar.get(Calendar.HOUR_OF_DAY), currentCalendar.get(Calendar.MINUTE),true).show();
             }
         });
-        //設定numberpicker
-        mNumberPicker = (NumberPicker)findViewById(R.id.numberpicker_SBP);
-        mNumberPicker.setMinValue(100);
-        mNumberPicker.setMaxValue(150);
-        mNumberPicker.setValue(120);
-        int nowValue = mNumberPicker.getValue();
-        mNumberPicker.setOnValueChangedListener(numberpicker_SBP_OnValueChange);
+
+//        tvTime= (TextView)findViewById(R.id.tvTime);
+//        bTimeChange = (Button)findViewById(R.id.tvChangeTime);
+//        Intent intent_SBP = getIntent();
+//        startActivityForResult(intent_SBP,REQUEST_CODE);
+//        bTimeChange.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //預設為系統時間
+//                final Calendar c = Calendar.getInstance();
+//                int hour = c.get(Calendar.HOUR_OF_DAY);
+//                int minute = c.get(Calendar.MINUTE);
+//                //抓現在時間
+//                new TimePickerDialog(DataRecord_BloodPressure_SBP.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker timePicker, int houreOfDay, int minute) {
+//                        tvTime.setText("現在時間是" + houreOfDay + ":" + minute);
+//                    }
+//                }, hour,minute,true).show();
+//            }
+//        });
+//        //設定numberpicker
+//        mNumberPicker = (NumberPicker)findViewById(R.id.numberpicker_SBP);
+//        mNumberPicker.setMinValue(100);
+//        mNumberPicker.setMaxValue(150);
+//        mNumberPicker.setValue(120);
+//        int nowValue = mNumberPicker.getValue();
+//        mNumberPicker.setOnValueChangedListener(numberpicker_SBP_OnValueChange);
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        switch(resultCode){
-            case REQUEST_CODE:
-                //int result = data.getIntExtra("bloodsugar");
-                break;
-        }
-    }
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        switch(resultCode){
+//            case REQUEST_CODE:
+//                //int result = data.getIntExtra("bloodsugar");
+//                break;
+//        }
+//    }
+//
+//    public void returnReply(View view){
+//        int reply = mNumberPicker.getValue();
+//        Intent replyIntent = new Intent();
+//        replyIntent.putExtra(EXTRA_REPLY,reply);
+//        setResult(RESULT_OK, replyIntent);
+//        finish();
+//    }
+//    private NumberPicker.OnValueChangeListener numberpicker_SBP_OnValueChange = new NumberPicker.OnValueChangeListener() {
+//        @Override
+//        public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
+//            int nowValue = mNumberPicker.getValue();
+//        }
+//    };
 
-    public void returnReply(View view){
-        int reply = mNumberPicker.getValue();
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY,reply);
-        setResult(RESULT_OK, replyIntent);
-        finish();
-    }
-    private NumberPicker.OnValueChangeListener numberpicker_SBP_OnValueChange = new NumberPicker.OnValueChangeListener() {
-        @Override
-        public void onValueChange(NumberPicker numberPicker, int oldValue, int newValue) {
-            int nowValue = mNumberPicker.getValue();
+    public static String setTimeForm(int currentTime) {
+        if (Integer.toString(currentTime).length()==1) {
+            return "0" + Integer.toString(currentTime);
+        } else {
+            return Integer.toString(currentTime);
         }
-    };
+    }
 }

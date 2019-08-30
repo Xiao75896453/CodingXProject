ffa9afb43b2c61f036d045232dd4d9449b942877
ElizaHuang <eliza85827@gmail.com>
2019-08-30 21:23:12 +0800

    修改新增數據頁面- 預設時間顯示目前手機時間   更改時間會跳出浮動視窗(可以用時鐘點選，或用鍵盤輸入)
    (java檔: recordDBP,  recordSBP, recordSugar)
    (setTime部分 有空的話可以整合成一個class)

--- app/src/main/java/com/example/codingxproject/SetTimeActivity.java
+++ app/src/main/java/com/example/codingxproject/SetTimeActivity.java
@@ -11,19 +11,20 @@ public class SetTimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_time);
-        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.picker);
-        //final String[] numbers = getResources().getStringArray(R.array.numbers);
-        Integer[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        numberPicker.setMinValue(0);
-        numberPicker.setMaxValue(numbers.length - 1);
-        //numberPicker.setDisplayedValues(numbers);
-        numberPicker.setValue(3); // 設定預設位置
-        numberPicker.setWrapSelectorWheel(false); // 是否循環顯示
-        numberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); // 不可編輯
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-            }
-        });
+
+//        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numberPicker1);
+//        //final String[] numbers = getResources().getStringArray(R.array.numbers);
+//        Integer[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        numberPicker.setMinValue(0);
+//        numberPicker.setMaxValue(numbers.length - 1);
+//        //numberPicker.setDisplayedValues(numbers);
+//        numberPicker.setValue(3); // 設定預設位置
+//        numberPicker.setWrapSelectorWheel(false); // 是否循環顯示
+//        numberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS); // 不可編輯
+//        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//            @Override
+//            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+//            }
+//        });
     }
 }

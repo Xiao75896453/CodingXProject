9f922b62f9ecc2fa4325414ae49a27b5e6129e7d
ElizaHuang <eliza85827@gmail.com>
2019-09-01 18:34:56 +0800

    進入HomPage頁，下方的navigation bar可以切換到各個主功能頁面 。
    - 輸入完三項資料按完確認後，會跳回原來homepage的頁面。
    - homepage預設畫面是分時段的藥物檢視。
    - navigation bar的身體數據，進去後有兩個button 增加身體數據或檢視歷史數據。(輸入身體數據還沒做)

---
+++ app/src/main/java/com/example/codingxproject/MainFunctionsWithBottomBar/AddOrViewDataFragment.java
@@ -0,0 +1,51 @@
+package com.example.codingxproject.MainFunctionsWithBottomBar;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.codingxproject.DataRecord.DataRecord_BloodPressure_DBP;
+import com.example.codingxproject.DataReview;
+import com.example.codingxproject.R;
+
+
+public class AddDataFragment extends Fragment {
+    public Button bAddRecord,bViewRecord;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_add_data,container,false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        bAddRecord=(Button)getActivity().findViewById(R.id.bAddRecord);
+        bViewRecord=(Button)getActivity().findViewById(R.id.bViewRecord);
+        View.OnClickListener listener=new View.OnClickListener() {
+            Intent intent;
+            @Override
+            public void onClick(View view) {
+                if(view.getId()==R.id.bAddRecord){
+                    intent=new Intent(getActivity(), DataRecord_BloodPressure_DBP.class);
+                }else if(view.getId()==R.id.bViewRecord){
+                    intent=new Intent(getActivity(), DataReview.class);
+                }
+                startActivity(intent);
+            }
+        };
+        bAddRecord.setOnClickListener(listener);
+        bViewRecord.setOnClickListener(listener);
+    }
+}
